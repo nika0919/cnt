291f4e5a9ccb48771e1a4df845b23e16454d4f15
OpakyJl <opakyjll@gmail.com>
2016-07-13 10:26:00 +0600

    FuckingFactory

---
+++ Calculator/Calculator.Tests/OneArgFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Calculator.OneArg;
+using Calculator.TwoArg;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    public class OneArgFactoryTests
+    {
+        [TestCase("sin", typeof(Sin))]
+        [TestCase("cos", typeof(Cos))]
+        [TestCase("tg", typeof(Tg))]
+        [TestCase("ctg", typeof(Ctg))]
+        [TestCase("ln", typeof(Ln))]
+        [TestCase("log", typeof(Log))]
+        [TestCase("sqrt", typeof(Sqrt))]
+        public void OneArgFactotyTest(string name, Type type)
+        {
+            var calculator = OneArgFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+
+    }
+}
---
+++ Calculator/Calculator.Tests/TwoArgFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Calculator.OneArg;
+using Calculator.TwoArg;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    public class TwoArgFactoryTests
+    {
+        [TestCase("add", typeof(Addition))]
+        [TestCase("minus", typeof(Deduction))]
+        [TestCase("multiply", typeof(Generation))]
+        [TestCase("division", typeof(Division))]
+        public void TwoArgFactotyTest(string name, Type type)
+        {
+            var calculator = TwoArgFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
